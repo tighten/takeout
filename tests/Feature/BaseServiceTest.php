3337010eb30767262f2dfad8d25ddc1def53e937
Matt Stauffer <matt@tighten.co>
2020-07-22 19:36:56 -0400

    Bring back install-service test in BaseServiceTest

--- tests/Feature/BaseServiceTest.php
+++ tests/Feature/BaseServiceTest.php
@@ -4,6 +4,11 @@ namespace Tests\Feature;
 
 use App\Services\MeiliSearch;
 use App\Services\MySql;
+use App\Shell\Shell;
+use Illuminate\Support\Str;
+use LaravelZero\Framework\Commands\Command;
+use Mockery as M;
+use Symfony\Component\Process\Process;
 use Tests\TestCase;
 
 class BaseServiceTest extends TestCase
@@ -23,5 +28,30 @@ class BaseServiceTest extends TestCase
         $this->assertEquals('latest', $tags[0]);
         $this->assertTrue(in_array('5.7', $tags));
         $this->assertCount(10, $tags);
+    }
+
+    /** @test */
+    function it_installs_services()
+    {
+        app()->instance('console', M::mock(Command::class, function ($mock) {
+            $mock->shouldIgnoreMissing();
+        }));
+
+        $this->mock(Shell::class, function ($mock) {
+            $process = M::mock(Process::class);
+            $process->shouldReceive('getExitCode')->twice()->andReturn(0);
+
+            $mock->shouldReceive('execQuietly')->once()->andReturn($process);
+            $mock->shouldReceive('exec')->once()->with(M::on(function ($arg) {
+                return Str::contains($arg, 'meilisearch');
+            }))->andReturn($process);
+        });
+
+        $this->mock(Docker::class, function ($mock) {
+            $mock->shouldReceive('isInstalled')->andReturn(true);
+        });
+
+        $service = app(MeiliSearch::Class); // Extends BaseService
+        $service->install();
     }
 }
