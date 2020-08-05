5f6eacfbf75b1e3087575d5f05b937615440b153
Jose Soto <josecanhelp@gmail.com>
2020-08-04 22:08:12 -0700

    Add Postgres

---
+++ app/Services/PostgreSql.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Services;
+
+class PostgreSql extends BaseService
+{
+    protected $imageName = 'postgres';
+    protected $defaultPort = 5432;
+    protected $prompts = [
+        [
+            'shortname' => 'volume',
+            'prompt' => 'What is the Docker volume name?',
+            'default' => 'postgres_data',
+        ],
+        [
+            'shortname' => 'root_password',
+            'prompt' => 'What will the root password be?',
+            'default' => 'password',
+        ],
+    ];
+
+    protected $installTemplate = '-p "$port":3306 \
+        -e POSTGRES_PASSWORD="$root_password" \
+        -v "$volume":/var/lib/postgresql/data \
+            "$organization"/"$image_name":"$tag"';
+}
