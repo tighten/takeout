26c49b5a25ab477aefbd5df477ff6bcc756d79a1
Rafael Lunardelli <rafael@devsquad.com>
2020-08-26 17:54:09 -0600

    Remove unecessary code

--- app/Services/MailHog.php
+++ app/Services/MailHog.php
@@ -12,16 +12,10 @@ class MailHog extends BaseService
                 'shortname' => 'web_port',
                 'prompt'    => 'What will the web port?',
                 'default'   => '8025',
-            ],
-            [
-                'shortname' => 'volume',
-                'prompt'    => 'What is the Docker volume name?',
-                'default'   => 'mailhog_data',
-            ],
+            ]
         ];
 
     protected $dockerRunTemplate = '-p "$port":1025 \
         -p "$web_port":8025 \
-        -v "$volume":/var/lib/mysql \
         "$organization"/"$image_name":"$tag"';
 }
