62f1d06bacf73838442255f9644d66229549265e
Owen Voke <development@voke.dev>
2020-08-27 09:08:51 +0100

    feat: add MariaDB service

---
+++ app/Services/MariaDB.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Services;
+
+class MariaDB extends BaseService
+{
+    protected $imageName = 'mariadb';
+    protected $defaultPort = 3306;
+    protected $prompts = [
+        [
+            'shortname' => 'volume',
+            'prompt' => 'What is the Docker volume name?',
+            'default' => 'mariadb_data',
+        ],
+        [
+            'shortname' => 'root_password',
+            'prompt' => 'What will the root password be?',
+            'default' => 'password',
+        ],
+    ];
+
+    protected $dockerRunTemplate = '-p "$port":3306 \
+        -e MYSQL_ROOT_PASSWORD="$root_password" \
+        -v "$volume":/var/lib/mysql \
+        "$organization"/"$image_name":"$tag"';
+}
