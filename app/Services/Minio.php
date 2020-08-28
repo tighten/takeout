b94768b99ac5d0906ded5dc888cb1eb6244a6fe1
Luís Dalmolin <luis.nh@gmail.com>
2020-08-28 07:28:43 -0300

    Added prompts for access and secret keys

--- app/Services/Minio.php
+++ app/Services/Minio.php
@@ -12,11 +12,21 @@ class Minio extends BaseService
             'prompt' => 'What is the Docker volume name?',
             'default' => 'minio_data',
         ],
+        [
+            'shortname' => 'access_key',
+            'prompt' => 'What will the access key for Minio be?',
+            'default' => 'minio',
+        ],
+        [
+            'shortname' => 'secret_key',
+            'prompt' => 'What will the secret key for Minio be?',
+            'default' => 'minio',
+        ],
     ];
 
     protected $dockerRunTemplate = '-p "$port":9000 \
-        -e MINIO_ACCESS_KEY=minio
-        -e MINIO_SECRET_KEY=minio
+        -e MINIO_ACCESS_KEY=$access_key
+        -e MINIO_SECRET_KEY=$secret_key
         -v "$volume":/data \
         "$organization"/"$image_name":"$tag"';
 }
