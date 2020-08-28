909931c67dbbeef639ce78f047e5e656c808ad5c
Matt Stauffer <matt@tighten.co>
2020-08-27 23:59:23 -0400

    Tweak MailHog syntax

--- app/Services/MailHog.php
+++ app/Services/MailHog.php
@@ -8,12 +8,12 @@ class MailHog extends BaseService
     protected $imageName = 'mailhog';
     protected $defaultPort = 1025;
     protected $prompts = [
-            [
-                'shortname' => 'web_port',
-                'prompt'    => 'What will the web port?',
-                'default'   => '8025',
-            ]
-        ];
+        [
+            'shortname' => 'web_port',
+            'prompt' => 'What will the web port be?',
+            'default' => '8025',
+        ]
+    ];
 
     protected $dockerRunTemplate = '-p "$port":1025 \
         -p "$web_port":8025 \
