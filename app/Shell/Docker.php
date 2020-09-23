6f3e195aaa51e4db53c3c6f4a72c04738b457683
Jose Soto <josecanhelp@gmail.com>
2020-09-23 01:21:45 -0700

    Only check for running containers when disabling services

--- app/Shell/Docker.php
+++ app/Shell/Docker.php
@@ -99,7 +99,7 @@ class Docker
 
     protected function allContainersRawOutput(): string
     {
-        $dockerProcessStatusString = 'docker ps -a --format "table {{.ID}}|{{.Names}}|{{.Status}}|{{.Ports}}"';
+        $dockerProcessStatusString = 'docker ps --format "table {{.ID}}|{{.Names}}|{{.Status}}|{{.Ports}}"';
         return trim($this->shell->execQuietly($dockerProcessStatusString)->getOutput());
     }
 
