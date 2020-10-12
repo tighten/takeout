3916269379dd1a223fefed0ebcd1cb7a1f8a9278
Grant <g.a.koehler@gmail.com>
2020-10-12 15:35:59 -0400

    Added clickhouse support

---
+++ app/Services/ClickHouse.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace App\Services;
+
+
+class ClickHouse extends BaseService
+{
+    protected $organization = 'yandex';
+    protected $imageName = 'clickhouse-server';
+    protected static $category = Category::DATABASE;
+    protected $defaultPort = 9000;
+    protected $prompts = [
+        [
+            'shortname' => 'http_port',
+            'prompt' => 'Which http host port would you like to use?',
+            'default' => '8123',
+        ],
+        [
+            'shortname' => 'volume',
+            'prompt' => 'What is the Docker volume name?',
+            'default' => 'clickhouse_data',
+        ],
+    ];
+
+    protected $dockerRunTemplate = '-p "${:port}":9000 \
+        -p "${:http_port}":8123 \
+        --ulimit nofile=262144:262144 \
+        --volume=${volume}:/var/lib/clickhouse \
+        "${:organization}"/"${:image_name}":"${:tag}"';
+
+}
