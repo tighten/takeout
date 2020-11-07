8b27136a695968e6840f56d11733569dba8aa99d
Matt Stauffer <matt@tighten.co>
2020-11-07 00:39:27 -0500

    Update quotes in list container Docker command

--- app/Shell/Docker.php
+++ app/Shell/Docker.php
@@ -55,7 +55,7 @@ class Docker
     public function takeoutContainers(): Collection
     {
         $process = sprintf(
-            'docker ps -a --filter "name=TO-" --format \'table %s|%s\'',
+            "docker ps -a --filter 'name=TO-' --format 'table %s|%s'",
             '{{.ID}}|{{.Names}}|{{.Status}}|{{.Ports}}',
             '{{.Label "com.tighten.takeout.Base_Alias"}}|{{.Label "com.tighten.takeout.Full_Alias"}}'
         );
