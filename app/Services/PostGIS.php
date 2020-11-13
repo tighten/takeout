2174e35af194bec025027425aafbec9dc94e2b1a
Matt Stauffer <matt@tighten.co>
2020-11-13 10:57:16 -0500

    Clean up indentation

--- app/Services/PostGIS.php
+++ app/Services/PostGIS.php
@@ -5,24 +5,24 @@ namespace App\Services;
 class PostGIS extends BaseService
 {
     protected static $displayName = 'PostGIS';
-
     protected static $category = Category::DATABASE;
 
-    protected $organization      = 'postgis';
-    protected $imageName         = 'postgis';
-    protected $defaultPort       = 5432;
-    protected $prompts           = [
+    protected $organization = 'postgis';
+    protected $imageName = 'postgis';
+    protected $defaultPort = 5432;
+    protected $prompts = [
         [
             'shortname' => 'volume',
-            'prompt'    => 'What is the Docker volume name?',
-            'default'   => 'postgis_data',
+            'prompt' => 'What is the Docker volume name?',
+            'default' => 'postgis_data',
         ],
         [
             'shortname' => 'root_password',
-            'prompt'    => 'What will the password for the `postgres` user be?',
-            'default'   => 'password',
+            'prompt' => 'What will the password for the `postgres` user be?',
+            'default' => 'password',
         ],
     ];
+
     protected $dockerRunTemplate = '-p "${:port}":5432 \
         -e POSTGRES_PASSWORD="${:root_password}" \
         -v "${:volume}":/var/lib/postgis/data \
