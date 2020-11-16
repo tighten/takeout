5853aa8210c1785d7ff8e89c464ad58ff9c42f5b
Matt Stauffer <matt@tighten.co>
2020-11-16 18:37:14 -0500

    Wip

--- src/shell/docker.ts
+++ src/shell/docker.ts
@@ -1,5 +1,13 @@
+const { execSync } = require('child_process');
+
 export default class Docker {
   static listTakeoutContainers() {
+    const output = execSync("docker ps -a --filter 'name=TO-' --format 'table {{.ID}}|{{.Names}}|{{.Status}}|{{.Ports}}|{{.Label \"com.tighten.takeout.Base_Alias\"}}|{{.Label \"com.tighten.takeout.Full_Alias\"}}'")
+    return this.dockerTableToArray(output.toString())
+  }
+
+  static dockerTableToArray(table) {
+    // @todo make this actually work
     return [
       {'Container Id': '123', Name: 'Best'},
       {'Container Id': '456', Name: 'Better'},
