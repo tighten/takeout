3aad3b0b9ae8f344831b366ca1b95677b9a38ab5
Matt Stauffer <matt@tighten.co>
2020-11-16 23:38:55 -0500

    Extract shell processing to separate helpers

--- src/shell/docker.ts
+++ src/shell/docker.ts
@@ -1,12 +1,7 @@
-const { execSync } = require('child_process')
+import {runAndParseAsJson} from './shell'
 
 export default class Docker {
-  static listTakeoutContainers(): Array<Record<string, any>> {
-    const output = execSync("docker ps -a --filter 'name=TO-' --format '{{ json . }}' --all")
-    return this.dockerJsonToArrayOfObjects(output.toString())
-  }
-
-  static dockerJsonToArrayOfObjects(table: string) {
-    return table.split(/\r?\n/).filter(Boolean).map((row: string) => JSON.parse(row))
+  static listTakeoutContainers() {
+    return runAndParseAsJson("docker ps -a --filter 'name=TO-' --format '{{ json . }}' --all")
   }
 }
