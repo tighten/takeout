8c8507fee6a3650e41a496c380817dec2a0336bb
Jose Soto <josecanhelp@gmail.com>
2020-11-20 20:02:21 -0800

    Add test for start command

--- test/commands/start.test.ts
+++ test/commands/start.test.ts
@@ -1,5 +1,15 @@
 import {expect, test} from '@oclif/test'
 import Docker from '../../src/shell/docker'
+const inquirer = require('inquirer')
+import {DockerContainer} from '../../src/types'
+
+const fakeTakeoutContainers: DockerContainer[] = [
+  {
+    ID: 'redisId',
+    Names: 'redis',
+    Status: 'Exited 0 minutes ago.',
+  },
+]
 
 describe('start', () => {
   test
@@ -15,7 +25,7 @@ describe('start', () => {
   .stub(Docker, 'startContainer', () => null)
   .stub(Docker, 'validContainerId', () => true)
   .command(['start', 'existentId'])
-  .it('start up a container and exit successfully', ctx => {
+  .it('starts a specific container', ctx => {
     expect(ctx.stdout).to.contain('Container successfully started.')
   })
 
@@ -25,5 +35,18 @@ describe('start', () => {
   .exit(0)
   .it('shows a help output', ctx => {
     expect(ctx.stdout).to.contain('takeout start')
+  })
+
+  test
+  .stub(Docker, 'listTakeoutContainers', () => {
+    return fakeTakeoutContainers
+  })
+  .stub(inquirer, 'prompt', () => Promise.resolve({containers: ['redis']}))
+  .stub(Docker, 'validContainerId', () => true)
+  .stub(Docker, 'startContainer', () => true)
+  .stdout()
+  .command(['start'])
+  .it('run start without arguments', async ctx => {
+    expect(ctx.stdout).to.contain('Container successfully started.')
   })
 })
