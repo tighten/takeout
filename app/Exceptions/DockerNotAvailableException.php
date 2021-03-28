1701deb6cce332809759b6fe30b2aa30e7138564
Ben Holmen <ben@benholmen.com>
2021-03-28 11:30:34 -0500

    Separate OS-specific messages into discrete methods

--- app/Exceptions/DockerNotAvailableException.php
+++ app/Exceptions/DockerNotAvailableException.php
@@ -12,34 +12,52 @@ class DockerNotAvailableException extends Exception
     {
         $console = app('console');
         $shell = app(Shell::class);
-        $environment = app(Environment::class);
 
         $console->line('');
         $console->line($shell->formatErrorMessage('Docker is not available.'));
         $console->line('');
 
-        if ($environment->isLinuxOs()) {
-            $console->line('Verify that the Docker service is running:');
-            $console->line('  sudo systemctl status docker');
-            $console->line('Start the Docker service:');
-            $console->line('  sudo systemctl start docker');
-
-            if (! $environment->userIsInDockerGroup()) {
-                $console->line('');
-                $console->line($shell->formatErrorMessage('You are not in the docker group.'));
-                $console->line('');
-                $console->line('This is required to run Docker as a non-root user.');
-                $console->line('Add your user to the docker group by running:');
-                $console->line('  sudo usermod -aG docker ${USER}');
-                $console->line('and restart your session.');
-            }
-        } elseif ($environment->isWindowsOs()) {
-            $console->line('Open Docker for Windows');
-            $console->line('to start the Docker service.');
-        } else {
-            $console->line('Open Docker for Mac or run:');
-            $console->line('  open --background -a Docker');
-            $console->line('to start the Docker service.');
+        if (in_array(PHP_OS_FAMILY, ['Darwin', 'Linux', 'Windows'])) {
+            $osSpecificHelp = 'helpFor' . ucfirst(PHP_OS_FAMILY);
+            $this->$osSpecificHelp();
         }
+    }
+
+    protected function helpForDarwin()
+    {
+        $console = app('console');
+
+        $console->line('Open Docker for Mac or run:');
+        $console->line('  open --background -a Docker');
+        $console->line('to start the Docker service.');
+    }
+
+    protected function helpForLinux()
+    {
+        $console = app('console');
+        $environment = app(Environment::class);
+        $shell = app(Shell::class);
+
+        $console->line('Verify that the Docker service is running:');
+        $console->line('  sudo systemctl status docker');
+        $console->line('Start the Docker service:');
+        $console->line('  sudo systemctl start docker');
+
+        if (! $environment->userIsInDockerGroup()) {
+            $console->line('');
+            $console->line($shell->formatErrorMessage('You are not in the docker group.'));
+            $console->line('');
+            $console->line('This is required to run Docker as a non-root user.');
+            $console->line('Add your user to the docker group by running:');
+            $console->line('  sudo usermod -aG docker ${USER}');
+            $console->line('and restart your session.');
+        }
+    }
+
+    protected function helpForWindows()
+    {
+        $console = app('console');
+
+        $console->line('Open Docker for Windows to start the Docker service.');
     }
 }
