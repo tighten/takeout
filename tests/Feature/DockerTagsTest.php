e64f1897e8a7c357db3a704d595f100abdcf0480
Matt Stauffer <matt@tighten.co>
2021-06-29 15:20:00 -0400

    Bump tags in fragile test

--- tests/Feature/DockerTagsTest.php
+++ tests/Feature/DockerTagsTest.php
@@ -48,8 +48,8 @@ class DockerTagsTest extends TestCase
         $dockerTags = app(DockerTags::class, ['service' => $postgres]);
         $tags = collect($dockerTags->getTags());
 
-        $this->assertEquals('alpine', $tags->first());
-        $this->assertEquals('9-alpine', $tags->last());
+        $this->assertEquals('latest', $tags->first());
+        $this->assertEquals('9-buster', $tags->last());
         $this->assertCount(10, $tags);
     }
 }
