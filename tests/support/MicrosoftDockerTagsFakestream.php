62e103aaa1888f516cfeead095d13294d04b1ed3
Kristin Collins <kristin@tighten.co>
2021-07-01 11:09:59 -0400

    Update test for PSR-4 and PSR-7.

--- tests/support/MicrosoftDockerTagsFakestream.php
+++ tests/support/MicrosoftDockerTagsFakestream.php
@@ -13,16 +13,21 @@ class MicrosoftDockerTagsFakestream extends Psr7Stream
 
     public function __toString()
     {
-        return json_encode([
-            'name' => 'mssql/server',
-            'tags' => [
-                '2017-CU1-ubuntu',
-                '2017-GDR3',
-                '2019-RC1',
-                '2019-GA-ubuntu-16.04',
-                '2024-GA-ubuntu-18.04',
-                'latest',
-            ],
-        ]);
+        try {
+            return json_encode([
+                'name' => 'mssql/server',
+                'tags' => [
+                    '2017-CU1-ubuntu',
+                    '2017-GDR3',
+                    '2019-RC1',
+                    '2019-GA-ubuntu-16.04',
+                    '2024-GA-ubuntu-18.04',
+                    'latest',
+                ],
+            ]);
+        }
+        catch (\Exception $e) {
+            return '';
+        }
     }
 }
