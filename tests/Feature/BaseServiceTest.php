992ffffdfe54d38eba9dbc8b9877cbc717c60b3a
Rias <hey@rias.be>
2021-08-13 09:18:29 +0200

    Adds a test for custom docker image support

--- tests/Feature/BaseServiceTest.php
+++ tests/Feature/BaseServiceTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Services\MeiliSearch;
+use App\Services\PostgreSql;
 use App\Shell\Docker;
 use App\Shell\Shell;
 use LaravelZero\Framework\Commands\Command;
@@ -61,6 +62,52 @@ class BaseServiceTest extends TestCase
         });
 
         $service = app(MeiliSearch::class); // Extends BaseService
+        $service->enable();
+    }
+
+    /** @test */
+    function it_can_receive_a_custom_image_in_the_tag()
+    {
+        $service = app(PostgreSql::class);
+
+        app()->instance('console', M::mock(Command::class, function ($mock) use ($service) {
+            $defaultPort = $service->defaultPort();
+            $mock->shouldReceive('ask')->with('Which host port would you like postgres to use?', $defaultPort)->andReturn(5432);
+            $mock->shouldReceive('ask')->with('Which tag (version) of postgres would you like to use?', 'latest')->andReturn('timescale/timescaledb:latest-pg12');
+            $mock->shouldReceive('ask')->with('What is the Docker volume name?', 'postgres_data')->andReturn('postgres_data');
+            $mock->shouldIgnoreMissing();
+        }));
+
+        $this->mock(Shell::class, function ($mock) {
+            $process = M::mock(Process::class);
+            $process->shouldReceive('isSuccessful')->andReturn(false);
+            $process->shouldReceive('getOutput')->andReturn('');
+
+            $mock->shouldReceive('execQuietly')->andReturn($process);
+        });
+
+        $this->mock(Docker::class, function ($mock) use ($service) {
+            $mock->shouldReceive('isInstalled')->andReturn(true);
+            $mock->shouldReceive('imageIsDownloaded')->andReturn(true);
+            $mock->shouldReceive('volumeIsAvailable')->andReturn(true);
+
+            // This is the actual assertion
+            $mock->shouldReceive('bootContainer')->with(
+                $service->sanitizeDockerRunTemplate($service->dockerRunTemplate()),
+                [
+                    'organization' => 'timescale',
+                    'image_name' => 'timescaledb',
+                    'port' => 5432,
+                    'tag' => 'latest-pg12',
+                    'volume' => 'postgres_data',
+                    'root_password' => null,
+                    'container_name' => 'TO--postgresql--latest-pg12--5432',
+                    'alias' => 'postgresql-latest-pg12',
+                ]
+            )->once();
+        });
+
+        $service = app(PostgreSql::class); // Extends BaseService
         $service->enable();
     }
 }
