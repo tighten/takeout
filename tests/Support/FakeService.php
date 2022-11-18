8fe809553314b023a7ef804c3e4855bc7ac14f6c
Tony Messias <tonysm@hey.com>
2022-11-18 16:22:02 -0300

    Adds some linting scripts

--- tests/Support/FakeService.php
+++ tests/Support/FakeService.php
@@ -8,11 +8,10 @@ use App\Services\Category;
 class FakeService extends BaseService
 {
     protected static $category = Category::CACHE;
-    protected $dockerTagsClass = FakeDockerTags::class;
 
+    protected $dockerTagsClass = FakeDockerTags::class;
     protected $organization = 'tighten';
     protected $imageName = '_test_image';
     protected $defaultPort = 12345;
-
     protected $dockerRunTemplate = '"${:organization}"/"${:image_name}":"${:tag}"';
 }
