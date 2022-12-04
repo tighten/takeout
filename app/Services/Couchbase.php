12a8674647cd6100e9f372e18c21ec527dce38b7
Rob Fonseca <robfonseca@gmail.com>
2022-12-04 08:18:38 -0500

    Remove build

--- app/Services/Couchbase.php
+++ app/Services/Couchbase.php
@@ -7,12 +7,21 @@ class Couchbase extends BaseService
     protected static $category = Category::DATABASE;
 
     protected $imageName = 'couchbase';
-    protected $defaultPort = 8091;
-    protected $prompts = [
+    protected $defaultPrompts = [
         [
-            'shortname' => 'encrypted_port',
-            'prompt' => 'Which encrypted port would you like %s to use?',
-            'default' => 11210,
+            'shortname' => 'ports',
+            'prompt' => 'Which port(s) would you like %s to use? (Must be 6 ports)',
+            'default' => '8091-8096',
+        ],
+        [
+            'shortname' => 'tag',
+            'prompt' => 'Which tag (version) of %s would you like to use?',
+            'default' => 'latest',
+        ],
+        [
+            'shortname' => 'encrypted_ports',
+            'prompt' => 'Which encrypted port(s) would you like %s to use? (Must be 2 ports)',
+            'default' => '11210-11211',
         ],
         [
             'shortname' => 'volume',
@@ -22,8 +31,8 @@ class Couchbase extends BaseService
     ];
 
     protected $dockerRunTemplate = '-d \
-        -p "${:port}":8091 \
-        -p "${:encrypted_port}":11210 \
+        -p "${:ports}":8091-8096 \
+        -p "${:encrypted_ports}":11210-11211 \
         -v "${:volume}":/opt/couchbase/var \
        "${:image_name}":"${:tag}"';
 
