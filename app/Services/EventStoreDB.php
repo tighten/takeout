942761c3111aebfd97fc978f8fb135d4c9667e83
Craig Morris <craig.morris@gosuperscript.com>
2023-03-14 19:17:45 +1100

    remove unused comment

--- app/Services/EventStoreDB.php
+++ app/Services/EventStoreDB.php
@@ -26,12 +26,6 @@ class EventStoreDB extends BaseService
         ],
     ];
 
-    /**
-     * The following only supports version 5.X.
-     *
-     * 20.6.0 requires various env variables to be set and forces HTTPS which is a pain in local dev.
-     * 20.6.1 can disable HTTPS but there's no auth which means most features are disabled.
-     */
     protected $dockerRunTemplate = '-p "${:port}":1113 \
         -p "${:web_port}":2113 \
         -v "${:volume}":/var/lib/eventstore \
