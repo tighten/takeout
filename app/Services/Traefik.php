29f6a8c3499af0cd4a78290380baa355c4426f68
Victor R <39545521+viicslen@users.noreply.github.com>
2023-04-25 23:19:05 -0400

    Add traefik service

---
+++ app/Services/Traefik.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Services;
+
+use App\Shell\Docker;
+use App\Shell\Environment;
+use App\Shell\Shell;
+use Exception;
+
+class Traefik extends BaseService
+{
+    protected static $category = Category::TOOLS;
+
+    protected $imageName = 'traefik';
+
+    protected $defaultTag = 'v2.10';
+
+    protected $defaultPort = 8080;
+
+    protected $prompts = [
+        [
+            'shortname' => 'config',
+            'prompt' => 'What is the configuration directory?',
+        ],
+    ];
+
+    protected $dockerRunTemplate = '-p "${:port}":8080 -p 80:80 -p 443:443 \
+        -v "${:config}":/etc/traefik \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        "${:organization}"/"${:image_name}":"${:tag}" \
+        --api.insecure=true --providers.docker=true --entryPoints.web.address=:80 --entryPoints.websecure.address=:443 --providers.file.directory=/etc/traefik/conf --providers.file.watch=true';
+
+
+    public function __construct(Shell $shell, Environment $environment, Docker $docker)
+    {
+        parent::__construct($shell, $environment, $docker);
+
+        $home = $this->homeDirectory();
+
+        $this->defaultPrompts = array_map(function ($prompt) {
+            if ($prompt['shortname'] === 'tag') {
+                $prompt['default'] = $this->defaultTag;
+            }
+
+            return $prompt;
+        }, $this->defaultPrompts);
+
+        $this->prompts = array_map(function ($prompt) use ($home) {
+            if ($prompt['shortname'] === 'config' && !empty($home)) {
+                $prompt['default'] = "$home/.config/traefik";
+            }
+
+            return $prompt;
+        }, $this->prompts);
+    }
+
+    protected function homeDirectory(): ?string {
+        // Cannot use $_SERVER superglobal since that's empty during UnitUnishTestCase
+        // getenv('HOME') isn't set on Windows and generates a Notice.
+        $home = getenv('HOME');
+
+        if (!empty($home)) {
+            // home should never end with a trailing slash.
+            $home = rtrim($home, '/');
+        }
+
+        elseif (!empty($_SERVER['HOMEDRIVE']) && !empty($_SERVER['HOMEPATH'])) {
+            // home on windows
+            $home = $_SERVER['HOMEDRIVE'] . $_SERVER['HOMEPATH'];
+            // If HOMEPATH is a root directory the path can end with a slash. Make sure
+            // that doesn't happen.
+            $home = rtrim($home, '\\/');
+        }
+
+        return empty($home) ? NULL : $home;
+    }
+
+    protected function prompts(): void
+    {
+        parent::prompts();
+
+        if (empty($this->promptResponses['config'])) {
+            throw new Exception('You must specify a configuration directory.');
+        }
+    }
+}
