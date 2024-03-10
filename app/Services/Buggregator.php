d1e798c4c77381bef50bd0ce8c84ee16b85f7d4a
Tony Messias <tonysm@hey.com>
2024-03-09 21:44:33 -0300

    Adds the network alias for ease calling this service from other containers on the same network

--- app/Services/Buggregator.php
+++ app/Services/Buggregator.php
@@ -29,12 +29,17 @@ class Buggregator extends BaseService
             'prompt' => 'What is the Monolog port?',
             'default' => '9913',
         ],
+        [
+            'shortname' => 'network_alias',
+            'prompt' => 'What network alias to you want to assign to this container? This alias can be used by other services on the same network.',
+            'default' => 'dump-server',
+        ],
     ];
 
     protected $dockerRunTemplate = '-p "${:port}":8000 \
         -p "${:smtp_port}":1025 \
         -p "${:var_dumper_port}":9912 \
         -p "${:monolog_port}":9913 \
-        --network-alias dump-server \
+        --network-alias "${:network_alias}" \
         "${:organization}"/"${:image_name}":"${:tag}"';
 }
