16b4817e7386116d0f1182938889b6ee1524b752
Tony Messias <tonysm@hey.com>
2024-09-20 19:00:32 -0300

    Fix CouchDB now seems to require user and password to run the image

--- app/Services/CouchDB.php
+++ app/Services/CouchDB.php
@@ -7,17 +7,31 @@ class CouchDB extends BaseService
     protected static $category = Category::DATABASE;
 
     protected $imageName = 'couchdb';
+
     protected $defaultPort = 5984;
+
     protected $prompts = [
         [
             'shortname' => 'volume',
             'prompt' => 'What is the Docker volume name?',
             'default' => 'couchdb_data',
         ],
+        [
+            'shortname' => 'user',
+            'prompt' => 'What is the CouchDB User?',
+            'default' => 'couchdb',
+        ],
+        [
+            'shortname' => 'password',
+            'prompt' => 'What is the CouchDB Password?',
+            'default' => 'password',
+        ],
     ];
 
     protected $dockerRunTemplate = '-p "${:port}":5984 \
         -v "${:volume}":/opt/couchdb/data \
+        -e COUCHDB_USER="${:user}" \
+        -e COUCHDB_PASSWORD="${:password}" \
         "${:organization}"/"${:image_name}":"${:tag}"';
 
     protected static $displayName = 'CouchDB';
