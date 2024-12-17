6b0c80832c48e4aa3d86a24b906d801abbaebd97
Tony Messias <tonysm@hey.com>
2024-12-17 19:52:05 -0300

    Use prompts in the invalid service shortname exception

--- app/Exceptions/InvalidServiceShortnameException.php
+++ app/Exceptions/InvalidServiceShortnameException.php
@@ -2,17 +2,14 @@
 
 namespace App\Exceptions;
 
-use App\Shell\Shell;
 use Exception;
+
+use function Laravel\Prompts\error;
 
 class InvalidServiceShortnameException extends Exception
 {
     public function render($request = null)
     {
-        $console = app('console');
-        $shell = app(Shell::class);
-
-        $console->line('');
-        $console->line($shell->formatErrorMessage($this->getMessage()));
+        error($this->getMessage());
     }
 }
